6bc8f7b2ed6dd6fd1c099b8b2fd1b3184188b425
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-10-26 13:42:15 -0400

    in the middle of adding gem depenencies, not quite working yet

--- lib/tardy_gardener.rb
+++ lib/tardy_gardener.rb
@@ -1,6 +1,12 @@
+# This appears to be the environment file.  Putting gems here. Seeing if this works...
+
+require 'pry'
+require 'require_all'
+
 require_relative "./tardy_gardener/version"
+require_all "./lib/tardy_gardener/"
 
 module TardyGardener
-  puts "hey you"
+  puts "hey you're in TardyGardener Module"
   # Your code goes here...
 end
